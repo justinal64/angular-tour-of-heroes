c85afd13ea58c06353a8ec49324dc451b5ce0584
Justin Leggett <jleggett@cjcweb.com>
2018-01-22 11:37:46 -0600

    removed test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
-import { TestComponent } from "./test/test.component";
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
-import { TestDetailComponent } from "./components/test-detail/test-detail.component";
 import { HeroService } from "./services/hero.service";
-import { MessagesComponent } from './components/messages/messages.component';
-import { MessageService } from './services/message.service';
+import { MessagesComponent } from "./components/messages/messages.component";
+import { MessageService } from "./services/message.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    TestComponent,
     HeroDetailComponent,
-    TestDetailComponent,
     MessagesComponent
   ],
   imports: [BrowserModule, FormsModule],
--- src/app/test.ts
+++
@@ -1,4 +0,0 @@
-export class Test {
-  id: number;
-  name: string;
-}
